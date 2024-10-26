9ac5c30682244801f18a4bad80118838ef286077
Rabin Rimal <alt_rabinrimal@yahoo.com>
2024-10-26 23:53:04 +1300

    added comments on BookingHandler class.

--- src/project2/RoomFactory.java
+++ src/project2/RoomFactory.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package project2;
 
 import Project1.DoubleRoom;
@@ -9,12 +5,9 @@ import Project1.Room;
 import Project1.SingleRoom;
 import Project1.Suite;
 
-/**
- *
- * @author emort
- */
 public class RoomFactory {
-
+    
+    // method to create a room of the specified type
     public static Room createRoom(String type, String roomNumber, double price) {
         switch (type.toLowerCase()) {
             case "single":
@@ -24,7 +17,7 @@ public class RoomFactory {
             case "suite":
                 return new Suite(roomNumber, price);
             default:
-                throw new IllegalArgumentException("Invalid room type: " + type);
+                throw new IllegalArgumentException("Invalid room type: " + type); // Handle invalid room type
         }
     }
 }
