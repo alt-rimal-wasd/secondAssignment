9dae7181fc92467e6ed110ed989671ccbf2b81ec
Rabin Rimal <alt_rabinrimal@yahoo.com>
2024-10-26 23:40:21 +1300

    added comments on HotelBookingMain class.

--- src/project2/HotelBookingMain.java
+++ src/project2/HotelBookingMain.java
@@ -1,17 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package project2;
 
 import java.sql.ResultSet;
 
-/**
- *
- * @author emort
- */
 public class HotelBookingMain {
-
     public static void main(String[] args) {
         Info info = new Info();
         info.connectHotelDB(); // Set up database tables
@@ -20,13 +11,7 @@ public class HotelBookingMain {
         // Retrieve available rooms
         try {
             ResultSet availableRooms = info.getAvailableRooms();
-            while (availableRooms.next()) {
-                System.out.println("Room ID: " + availableRooms.getString("ROOMID"));
-                System.out.println("Type: " + availableRooms.getString("TYPE"));
-                System.out.println("Price: " + availableRooms.getDouble("PRICE"));
-                System.out.println("Available: " + availableRooms.getBoolean("AVAILABLE"));
-                System.out.println();
-            }
+           
         } catch (Exception e) {
             e.printStackTrace();
         }
