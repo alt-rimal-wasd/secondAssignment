8ee30e9307ac411de2a82ccba1c1e0103ca8f449
Rabin Rimal <alt_rabinrimal@yahoo.com>
2024-10-27 13:20:57 +1300

    removed the debugging print statement

--- src/project2/HotelBookingMain.java
+++ src/project2/HotelBookingMain.java
@@ -1,20 +1,10 @@
 package project2;
 
-import java.sql.ResultSet;
-
 public class HotelBookingMain {
     public static void main(String[] args) {
         Info info = new Info();
         info.connectHotelDB(); // Set up database tables
         System.out.println("Database setup complete.");
-
-        // Retrieve available rooms
-        try {
-            ResultSet availableRooms = info.getAvailableRooms();
-           
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
         DBManager.getInstance().closeConnections();
     }
